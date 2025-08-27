a7f1bfcfa1ddb6298e969f22eb717e92dad1359f
Renato Muñoz Villafana <145702251+Holapex9@users.noreply.github.com>
2025-08-26 22:00:15 -0500

    Update run_backend_ngrok.bat

--- Backend/run_backend_ngrok.bat
+++ Backend/run_backend_ngrok.bat
@@ -4,7 +4,7 @@ echo 🚀 Iniciando Backend con Git Bash...
 :: Abrir Git Bash y ejecutar Spring Boot
 start "" "E:\Git\bin\bash.exe" -lc "cd '%~dp0' && ./mvnw spring-boot:run"
 
-timeout /t 10 >nul
+timeout /t 15 >nul
 
 echo 🌐 Iniciando Ngrok en puerto 8080 con dominio estatico...
 start cmd /k "ngrok http --url=touched-included-elephant.ngrok-free.app 8080"
